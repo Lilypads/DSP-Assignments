43fcab549d62f5cba042500029f963abcdc6037d
Lilypads <67157765+Lilypads@users.noreply.github.com>
2020-10-08 15:22:39 +0100

    edit

--- voice_enhancer.py
+++ voice_enhancer.py
@@ -6,11 +6,61 @@ from scipy.io import wavfile
 from scipy.io.wavfile import write
 
 # Read WAV file Frequency sample: 44100 Hz and Data
-fs, data = wavfile.read('seashells.wav')
+fs, data = wavfile.read('original.wav')
 print(fs)
 
 pyplot.figure(1)
 pyplot.plot(data)
-plot.axes.Axes.set_xlabel('time')
-plot.axes.Axes.set_ylabel('amplitude')
-plot.axes.Axes.set_title('Time Domain')
+
+xf = np.fft.fft(data)   # Fourier Transform
+
+
+# DB Conversion (Im not sure about this either)
+dbs = 20*np.log10(xf)
+pyplot.figure(2)
+pyplot.plot(dbs)
+
+
+length= len(xf)
+arraysize = len(dbs)
+total = arraysize/fs
+time = np.linspace(0,total,arraysize)
+
+# Db VS time Plot
+pyplot.figure(3)
+pyplot.plot(time,abs(dbs))
+
+# Freq vs Db Plot 
+freq = np.linspace(0,fs,length)
+pyplot.figure(4)
+pyplot.plot(freq,abs(dbs))
+pyplot.xscale('log')        # Log Scale
+pyplot.yscale('log')
+
+f1 = len(dbs)/fs*6000
+f2 = len(dbs)/fs*10000
+f3 = len(dbs) - int(f1)
+f4 = len(dbs) - int(f2)
+
+print(f1)
+print(f2)
+print(f3)
+print(f4)
+
+dbs[int(f1):int(f2)+1] = dbs[int(f1):int(f2)+1]*2
+
+
+pyplot.figure(5)
+pyplot.plot(freq,abs(dbs))
+pyplot.xscale('log')        # Log Scale
+pyplot.yscale('log')
+
+# Convert back to non db units
+nondb = 10 ** (dbs / 20)
+
+dbfilter = np.fft.ifft(nondb)
+    
+pyplot.figure(6)
+pyplot.plot(nondb)
+
+write('improved.wav', fs, dbfilter.astype(data.dtype)) 
