58eef0e02545df83a6a64ddf6f6975690c63951a
Lilypads <67157765+Lilypads@users.noreply.github.com>
2020-10-08 19:14:29 +0100

    s

--- voice_enhancer.py
+++ voice_enhancer.py
@@ -1,70 +1,74 @@
-import scipy.io
 import numpy as np
-#import matplotlib.pylab as plot
 from matplotlib import pyplot
 from scipy.io import wavfile
 from scipy.io.wavfile import write
 
 # Read WAV file Frequency sample: 44100 Hz and Data
-fs, data = wavfile.read('original.wav')
+fs, data = wavfile.read(r'C:\Users\Visitor\Desktop\DSP\Voice2.wav')
+
+print(len(data))
+
+xf = np.fft.fft(data)     # Fourier Transform 
+
+xfourier = xf/len(data)   # Fourier Transform Normalised
+# DB Conversion 
+dbs = 20*np.log10(abs(xfourier))
+
+print(len(xf))
 
 
-pyplot.figure(1)
-pyplot.ylabel('amplitude')
-pyplot.xlabel('time')
-pyplot.plot(data)
-
-'''
-xf = np.fft.fft(data)   # Fourier Transform
-
-
-# DB Conversion (Im not sure about this either)
-dbs = 20*np.log10(data)
-pyplot.figure(2)
-pyplot.plot(dbs,tlog)
-
-
-length= len(xf)
 arraysize = len(dbs)
 total = arraysize/fs
 time = np.linspace(0,total,arraysize)
-
-# Db VS time Plot
-pyplot.figure(3)
-pyplot.plot(time,abs(dbs))
+# Plot Time file
+pyplot.figure(1)
+pyplot.title('wavfile')
+pyplot.plot(time,data)
+pyplot.xlabel('Time(s)')
 
 # Freq vs Db Plot 
-freq = np.linspace(0,fs,length)
-pyplot.figure(4)
-pyplot.plot(freq,abs(dbs))
+freq = np.linspace(0,fs,arraysize)
+pyplot.figure(3)
+pyplot.plot(freq, dbs)
+pyplot.xlim(1,fs/2)
+pyplot.title('freq vs DB')
+pyplot.xlabel('Frequency(hz)')
+pyplot.ylabel('Amplitude(db)')
 pyplot.xscale('log')        # Log Scale
-pyplot.yscale('log')
+#pyplot.yscale('log')
 
-f1 = len(dbs)/fs*6000
-f2 = len(dbs)/fs*10000
-f3 = len(dbs) - int(f1)
-f4 = len(dbs) - int(f2)
+pyplot.figure(4)
+pyplot.plot(freq, dbs)
+#pyplot.xlim(0,22050)
+pyplot.title('freq vs DB')
+pyplot.xlabel('Frequency(hz)')
+pyplot.ylabel('Amplitude(db)')
+pyplot.xscale('log')        # Log Scale
+#pyplot.yscale('log')
+
+# Find the array around 6000 - 10000 Hz
+f1 = int(len(xf)/fs*6000)
+f2 = int(len(xf)/fs*10000)
+f3 = len(xf) - f1
+f4 = len(xf) - f2
 
 print(f1)
 print(f2)
 print(f3)
 print(f4)
 
-dbs[int(f1):int(f2)+1] = dbs[int(f1):int(f2)+1]*2
+gain = 8
 
+# 6000 - 10000 hz is gained by
+xf[f1:f2+1] *= 8
+xf[f4:f3+1] *= 8
 
 pyplot.figure(5)
-pyplot.plot(freq,abs(dbs))
-pyplot.xscale('log')        # Log Scale
-pyplot.yscale('log')
+pyplot.plot(xf)
 
-# Convert back to non db units
-nondb = 10 ** (dbs / 20)
+xffiltered = np.fft.ifft(xf)
 
-dbfilter = np.fft.ifft(nondb)
-    
 pyplot.figure(6)
-pyplot.plot(nondb)
-
-write('improved.wav', fs, dbfilter.astype(data.dtype)) 
-'''
+pyplot.plot(xffiltered)
+    
+write('output4.wav', fs, xffiltered.astype(data.dtype)) 
